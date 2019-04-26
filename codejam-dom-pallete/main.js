3d2493836ff9050a5e648303f40bf68ef17d4a81
Pytniza <ip.web.so@gmail.com>
2019-04-26 11:05:26 +0300

    feat: add tool 'Choose color'

--- codejam-dom-pallete/main.js
+++ codejam-dom-pallete/main.js
@@ -1,11 +1,12 @@
 let color = 'red';
 let tool = 'Paint bucket';
 const canvas = document.getElementById('canvas');
+
 canvas.addEventListener('click', function (event) {
     let target = event.target;
     if (target.className !== 'column') return;
     if (tool === 'Paint bucket') changeBackground(target);
-    if (tool === 'Choose color') return;
+    if (tool === 'Choose color') chooseColor(target);
     if (tool === 'Move') return;
     if (tool === 'Transform') return;
 
@@ -15,10 +16,24 @@ function changeBackground(element) {
     element.style.background = color;
 }
 
+// colors 
 const colors = document.getElementById('colors')
-
 colors.addEventListener('click', function (event) {
     let target = event.target;
     if (target.tagName != 'BUTTON') return;
     color = target.innerHTML;
+    tool = 'Paint bucket';
 })
+
+//tools
+const tools = document.getElementById('tools')
+tools.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target.tagName != 'BUTTON') return;
+    tool = target.innerHTML;
+})
+
+//Choose color
+function chooseColor(target){
+    alert(`Currect color: ${target.style.background}`)
+}
